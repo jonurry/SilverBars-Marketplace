f420c271956dfc8db8188ab5fae51f505850f3e8
Jon Urry <jon@urry.me>
2019-01-18 10:23:54 +0000

    LiveOrderBoard should register new Orders
    Calling #registerOrder should create a new Order for either
    BUY or SELL based on the passed parameters.

--- __tests__/liveOrderBoard.js
+++ __tests__/liveOrderBoard.js
@@ -1,12 +1,37 @@
 import LiveOrderBoard from '../src/liveOrderBoard.js'
 
-const orderFactory = jest.fn()
+const buyOrder = {
+  userId: 1,
+  quantity: 2.5,
+  price: 303,
+  type: 'BUY'
+}
+const sellOrder = {
+  userId: 2,
+  quantity: 1.7,
+  price: 307,
+  type: 'SELL'
+}
+const buyOrderFactory = jest.fn(() => buyOrder)
+const sellOrderFactory = jest.fn(() => sellOrder)
 
 describe('Live Order Board', () => {
   describe('#constructor', () => {
     test('it should take an Order factory method as the first parameter', () => {
-      const liveOrderBoard = new LiveOrderBoard(orderFactory)
-      expect(liveOrderBoard.orderFactory).toBe(orderFactory)
+      const liveOrderBoard = new LiveOrderBoard(buyOrderFactory)
+      expect(liveOrderBoard.orderFactory).toBe(buyOrderFactory)
+    })
+  })
+
+  describe('#registerOrder', () => {
+    test('it should register a BUY Order', () => {
+      const liveOrderBoard = new LiveOrderBoard(buyOrderFactory)
+      expect(liveOrderBoard.registerOrder(buyOrder)).toBe(buyOrder)
+    })
+
+    test('it should register a SELL Order', () => {
+      const liveOrderBoard = new LiveOrderBoard(sellOrderFactory)
+      expect(liveOrderBoard.registerOrder(sellOrder)).toBe(sellOrder)
     })
   })
 })
--- src/liveOrderBoard.js
+++ src/liveOrderBoard.js
@@ -2,4 +2,8 @@ export default class LiveOrderBoard {
   constructor(orderFactory) {
     this.orderFactory = orderFactory
   }
+
+  registerOrder(userId, quantity, price, type) {
+    return this.orderFactory(userId, quantity, price, type)
+  }
 }
