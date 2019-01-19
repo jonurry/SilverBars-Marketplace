86985b3c0bfed03d0324e33d5343af9b6c94f4b4
Jon Urry <jon@urry.me>
2019-01-19 11:59:34 +0000

    Summary should show multiple BUY and SELL orders in the correct order with cumulative quantities for same prices

--- __tests__/integration.js
+++ __tests__/integration.js
@@ -19,8 +19,30 @@ describe('Integration Tests', () => {
     const board = new LiveOrderBoard(createOrder, printer)
     board.registerOrder(1, 2.2, 301, 'BUY')
     board.registerOrder(2, 3.0, 304, 'SELL')
-    board.cancelOrder(1)
+    board.registerOrder(3, 1.3, 305, 'BUY')
+    board.cancelOrder(2)
+    board.cancelOrder(2)
     board.cancelOrder(1)
     expect(board.summary()).toEqual('')
+  })
+
+  test('Summary should display BUY and SELL orders in the correct order', () => {
+    const printer = new OrderBoardPrinter()
+    const board = new LiveOrderBoard(createOrder, printer)
+    board.registerOrder(4, 2.2, 301, 'BUY')
+    board.registerOrder(1, 3.5, 306, 'SELL')
+    board.registerOrder(2, 1.2, 310, 'SELL')
+    board.registerOrder(3, 2.9, 301, 'BUY')
+    board.registerOrder(3, 1.5, 307, 'SELL')
+    board.registerOrder(2, 1.6, 303, 'BUY')
+    board.registerOrder(4, 2.0, 306, 'SELL')
+    board.registerOrder(1, 3.5, 305, 'BUY')
+    board.registerOrder(4, 0.8, 303, 'BUY')
+    expect(board.summary()).toEqual(`BUY: 3.5kg for £305
+BUY: 2.4kg for £303
+BUY: 5.1kg for £301
+SELL: 5.5kg for £306
+SELL: 1.5kg for £307
+SELL: 1.2kg for £310`)
   })
 })
